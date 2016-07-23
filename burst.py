eb41699802c0048cefacf96b7adba2d28d47eb3c
Andy Dietz <adietz@gmail.com>
2016-07-22 20:42:26 -0600

    Initial Commit

---
+++ burst.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+#
+#
+
+import sys, argparse, logging, re
+from PyPDF2 import PdfFileWriter, PdfFileReader
+from collections import defaultdict
+
+
+# Gather our code in a main() function
+def main(args):
+
+    burstTag = '{(.+?)}'
+    if args.tag is not None:
+        burstTag = args.tag
+    print "Using Burst Tag: ", burstTag, ' on ', args.inputpdf
+    input1 = PdfFileReader(open(args.inputpdf, 'rb'))
+    previousTag = "TAGNOTFOUND"
+    currentTag = ""
+    output = PdfFileWriter()
+    outputpages = defaultdict(list)
+    for page in input1.pages:
+        pagecontent = page.extractText()
+        tagSearch = re.search(burstTag, pagecontent)
+        if tagSearch:
+            currentTag = str(tagSearch.group(1))
+            previousTag = currentTag
+        else: #no tag was found, reuse the previous page's tag
+            currentTag = previousTag
+        outputpages[currentTag].append(page)
+    for tag,pg in outputpages.iteritems():
+        print tag
+        output = PdfFileWriter()
+        for outpage in pg:
+            output.addPage(outpage)
+        outputStream = file(tag + '-' + args.inputpdf, "wb")
+        output.write(outputStream)
+
+# Standard boilerplate to call the main() function to begin
+# the program.
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Bursts a PDF File to multiple files, appending the burst tag to the front of the filename.",
+        epilog="As an alternative to the commandline, params can be placed in a file, one per line, and specified on the commandline like '%(prog)s @params.conf'.",
+        fromfile_prefix_chars='@')
+    # TODO Specify your real parameters here.
+    parser.add_argument(
+        "inputpdf",
+        help="PDF file you want to burst",
+        metavar="ARG")
+    parser.add_argument(
+        "-t",
+        "--tag",
+        help='regex representation, including a match group, to use as burst tag.  default will be {(.+?)}',
+        metavar='ARG')
+    args = parser.parse_args()
+    main(args)
